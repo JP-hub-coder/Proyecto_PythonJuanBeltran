5a9374c353e56cfa081b4d31bb01d5478c2dbfc0
JP-hub-coder <238061217+JP-hub-coder@users.noreply.github.com>
2025-12-03 23:53:56 -0500

    Update utils.py
    Era mas facil uno por uno, mi mente no dió pa más

--- utils/utils.py
+++ utils/utils.py
@@ -19,40 +19,89 @@ def findFecha(datalist, key, valor):
             info.append(item)
     return info
 
-def totalSemanal(lista):
+def totalSemanal(datalist):
     hoy = datetime.now()
     hace_7_dias = hoy - timedelta(days=7)
 
     total = 0
 
-    for gasto in lista:
+    for gasto in datalist:
         fecha_gasto = datetime.strptime(gasto["Fecha"], "%d/%m/%Y")
         if fecha_gasto >= hace_7_dias:
             total += gasto["Valor"]
 
     print(f"Total gastado en los últimos 7 días: ${total}")
 
-def totalDiario(lista):
+def totalDiario(datalist):
     hoy = datetime.now().strftime("%d/%m/%Y")
     total = 0
 
-    for gasto in lista:
+    for gasto in datalist:
         if gasto["Fecha"] == hoy:
             total += gasto["Valor"]
 
     print(f"Total gastado HOY ({hoy}): ${total}")
 
-def totalMensual(lista):
+def totalMensual(datalist):
     hoy = datetime.now()
     mesActual = hoy.month
     añoActual = hoy.year
 
     total = 0
 
-    for gasto in lista:
+    for gasto in datalist:
         fechaGasto = datetime.strptime(gasto["Fecha"], "%d/%m/%Y")
 
         if fechaGasto.month == mesActual and fechaGasto.year == añoActual:
             total += gasto["Valor"]
 
     print(f"Total gastado este mes: ${total}")
+
+def reporteDiario(datalist):
+    hoy = datetime.now().strftime("%d/%m/%Y")
+    total = 0
+
+
+    print("== R E P O R T E  D I A R I O ==")
+    print(f"{hoy}")
+    TEMPLATE = "{:<12}{:<22}{:<17}{:>}"
+    TEMPLATE_TITLE = "{:^6}{:^22}{:^23}{:^}"
+    print(TEMPLATE_TITLE.format("VALOR", "CATEGORÍA", "DESCRIPCIÓN", "FECHA"))
+
+    for gasto in datalist:
+        if gasto["Fecha"] == hoy:
+            print(TEMPLATE.format(gasto["Valor"], gasto["Categoria"], gasto["Descripcion"], gasto["Fecha"]))
+            total += gasto["Valor"]
+
+def reporteSemanal(datalist):
+    hoy = datetime.now()
+    hace7dias = hoy - timedelta(days=7)
+    total = 0
+
+    print("== R E P O R T E  S E M A N A L ==")
+    TEMPLATE = "{:<12}{:<22}{:<17}{:>}"
+    TEMPLATE_TITLE = "{:^6}{:^22}{:^23}{:^}"
+    print(TEMPLATE_TITLE.format("VALOR", "CATEGORÍA", "DESCRIPCIÓN", "FECHA"))
+
+    for gasto in datalist:
+        fechaGasto = datetime.strptime(gasto["Fecha"], "%d/%m/%Y")
+        if fechaGasto >= hace7dias:
+            print(TEMPLATE.format(gasto["Valor"], gasto["Categoria"], gasto["Descripcion"], gasto["Fecha"]))
+            total += gasto["Valor"]
+
+def reporteMensual(datalist):
+    hoy = datetime.now()
+    mesActual = hoy.month
+    total = 0
+
+    print("== R E P O R T E  M E N S U A L ==")
+    TEMPLATE = "{:<12}{:<22}{:<17}{:>}"
+    TEMPLATE_TITLE = "{:^6}{:^22}{:^23}{:^}"
+    print(TEMPLATE_TITLE.format("VALOR", "CATEGORÍA", "DESCRIPCIÓN", "FECHA"))
+    for gasto in datalist:
+        fechaGasto = datetime.strptime(gasto["Fecha"], "%d/%m/%Y")
+
+        if fechaGasto.month == mesActual:
+            print(TEMPLATE.format(gasto["Valor"], gasto["Categoria"], gasto["Descripcion"], gasto["Fecha"]))
+            total += gasto["Valor"]
+
